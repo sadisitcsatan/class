3019befd593e20a312def31176d83358909f4d70
Chris Scott <chrscott@csumb.edu>
2019-09-05 11:58:11 -0700

    fixing lab1 file name

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//route to render HTMl
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
